3e1995c10ad42011247523b8dbbb8aee63688acd
fashevskij <68337800+fashevskij@users.noreply.github.com>
2021-02-09 15:41:09 +0200

    add project card

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app" class="container">
     <div id="nav" class="navbar navbar-light">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link> |
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
       <router-link to="/portfolio">Portfolio</router-link>
     </div>
-    <router-view/>
+    <router-view class="v"/>
   </div>
 </template>
 
@@ -16,16 +16,22 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: darkslategrey;
+  border-radius: 20px;
+  padding: 20px;
 }
 
 #nav {
   padding: 30px;
+
+  border-radius: 20px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  font-size: x-large;
+  color: #009178;
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,12 +1,53 @@
 <template>
-<div>
-  page 4
-</div>
+  <div class="row mt-5">
+    <div class="card m-2" style="width: 18rem;" v-for="card in cards">
+      <img src="" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">{{ card.title }}</h5>
+        <p class="card-text">{{ card.text }}</p>
+        <a href="#" class="btn btn-primary">{{ card.btn }}</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      cards:[
+        {
+          title: "1",
+          photo: '',
+          text: '1',
+          btn: '1',
+        },
+        {
+          title: "2",
+          photo: '',
+          text: '2',
+          btn: '2',
+        },
+        {
+          title: "3",
+          photo: '',
+          text: '3',
+          btn: '3',
+        },
+        {
+          title: "4",
+          photo: '',
+          text: '4',
+          btn: '4',
+        },
+        {
+          title: "5",
+          text: '5',
+          btn: '5',
+        }
+      ]
+    }
+  }
 }
 </script>
 
